ad197fb4d2ebcef16557390392ef1fbcba9e3ade
romnkrki <romnkrki@gmail.com>
2021-09-23 21:13:41 +0545

    changing class in cs

--- Sorting/Bubble-Sort/BubbleSort.cs
+++ Sorting/Bubble-Sort/BubbleSort.cs
@@ -1,35 +1,41 @@
 namespace Sorting
 {
-    public static int[] BubbleSort(int[] arr)
+    using System;
+
+    public class Program
     {
-        for (int i = 0; i < arr.Length; i++)
+        public static int[] BubbleSort(int[] arr)
         {
-            for (int j = 0; j < arr.Length - 1; j++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                if (arr[j] > arr[j + 1])
+                for (int j = 0; j < arr.Length - 1; j++)
                 {
-                    swap(j, j + 1, arr);
+                    if (arr[j] > arr[j + 1])
+                    {
+                        swap(j, j + 1, arr);
+                    }
                 }
             }
+            return arr;
         }
-        return arr;
-    }
-    public static void swap(int i, int j, int[] array)
-    {
-        int temp = array[j];
-        array[j] = array[i];
-        array[i] = temp;
-    }
-    public static void PrintArray(int[] arr)
-    {
+        public static void PrintArray(int[] arr)
+        {
 
-        Console.WriteLine("[{0}]", string.Join(", ", arr));
-    }
-    public static void Main()
-    {
-        // Time - O(n^2)
-        // Space - O(1)
-        var a = BubbleSort(new int[] { 1, 9, 18, 5, 10, 3, 4 });
-        PrintArray(a);
+            Console.WriteLine("[{0}]", string.Join(", ", arr));
+        }
+        public static void swap(int i, int j, int[] array)
+        {
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+        public static void Main()
+        {
+            // Time - O(n^2)
+            // Space - O(1)
+            var a = BubbleSort(new int[] { 1, 9, 18, 5, 10, 3, 4 });
+            PrintArray(a);
+
+        }
     }
 }
