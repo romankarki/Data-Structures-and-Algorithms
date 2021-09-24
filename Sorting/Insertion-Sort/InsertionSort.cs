8078e79d6d17c2e37fa2e6296f1dca407d4123e5
romnkrki <romnkrki@gmail.com>
2021-09-24 22:39:51 +0545

    insertion sort in c#

---
+++ Sorting/Insertion-Sort/InsertionSort.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class InsertionSort
+{
+    public static int[] InsertionSortArray(int[] arr)
+    {
+        for (var i = 1; i < arr.Length; i++)
+        {
+            var j = i;
+            while (j > 0 && arr[j] < arr[j - 1])
+            {
+                swap(j, j - 1, arr);
+                j = j - 1;
+            }
+        }
+        return arr;
+    }
+    public static void swap(int i, int j, int[] arr)
+    {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void Main()
+    {
+        int[] a = new int[] { 3, 2, 4, 5, 1, 12, 19, 11 };
+        var result = InsertionSortArray(a);
+        //printing Results
+        Console.WriteLine("[{0}]", string.Join(", ", result));
+    }
+}
